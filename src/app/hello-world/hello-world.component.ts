fd26a5685cffa9dd42a0824930029e9797b9e62c
plug-in-pow <tripathimayank36@gmail.com>
2021-07-31 22:13:02 +0530

    Created a Hello World Component

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent implements OnInit {
+
+  @Input() name:string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
